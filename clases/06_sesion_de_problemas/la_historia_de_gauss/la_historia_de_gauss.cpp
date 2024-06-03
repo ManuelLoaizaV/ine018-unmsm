49347e90f9dd9b928ff3cf3191bd5e3681c0e18e
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-06-03 00:38:29 -0500

    gauss

---
+++ clases/06_sesion_de_problemas/la_historia_de_gauss/la_historia_de_gauss.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n = 100;
+    int suma = n * (n + 1) / 2;
+    cout << "La suma de 1 + ... + 100 es " << suma << endl;
+    return 0;
+}
