9eeada13acb78ab8bac4ce787b9695cbf70c2b89
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-06-02 20:52:04 -0500

    Upper and lower case

--- clases/12_sesion_de_problemas/minusculas/minusculas.cpp
+++ clases/12_sesion_de_problemas/minusculas/minusculas.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-string Mayusculas(string s) {
+string Minusculas(string s) {
     // Obtenemos el tamaño de la cadena
     int n = s.size();
 
     // Para cada caracter de la cadena
     for (int i = 0; i < n; i++) {
-        // Si es que este es minúscula, lo convertimos a mayúscula
-        if ('a' <= s[i] && s[i] <= 'z') {
-            s[i] = (s[i] - 'a') + 'A';
+        // Si es que este es mayúscula, lo convertimos a minúscula
+        if ('A' <= s[i] && s[i] <= 'Z') {
+            s[i] = (s[i] - 'A') + 'a';
         }
     }
 
@@ -18,11 +18,11 @@ string Mayusculas(string s) {
 }
 
 int main(void) {
-    cout << Mayusculas("Manuel Loaiza") << endl;
-    cout << Mayusculas("alex cisneros") << endl;
-    cout << Mayusculas("ARIADNA ROSSA") << endl;
-    cout << Mayusculas("pi=3.1415926535897932") << endl;
+    cout << Minusculas("Manuel Loaiza") << endl;
+    cout << Minusculas("alex cisneros") << endl;
+    cout << Minusculas("ARIADNA ROSSA") << endl;
+    cout << Minusculas("pi=3.1415926535897932") << endl;
     string s = "UnMsm2024";
-    cout << Mayusculas(s) << endl;
+    cout << Minusculas(s) << endl;
     return 0;
 }
