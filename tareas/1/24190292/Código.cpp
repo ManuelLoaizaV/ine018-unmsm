f6aa546be57f1d797d6c64ea7cdd84a96ef584b6
Rodrigo <jamirvac@gmail.com>
2024-06-07 13:23:39 -0500

    comiteando todo lo que quiero subir a mi fork

---
+++ tareas/1/24190292/Código.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    int n=1;
+    while(n<=3){
+        n++;
+        cin>>a;
+        cout<<"omla profe :3"<<endl;
+        cout<<"omla"<<" "<<"homla:"<<endl;
+        while(a<70){
+        a*=2;
+    }
+    cout<<a<<endl;
+    }
+    return 0;
+}
