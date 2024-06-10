489dccc4b5096ee1840b15a7c537cdaa7998c32e
Ivan-v2024 <ivanlichess.org@gmail.com>
2024-06-10 15:40:50 -0500

    Create ejecucion_del_codigo.cpp

---
+++ tareas/1/23190395/ejecucion_del_codigo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string pieza;
+
+    cout << "Ingresa una pieza de ajedrez: ";
+    cin >> pieza;
+
+    cout << "Has ingresado la pieza: " << pieza << endl;
+
+    return 0;
+}
