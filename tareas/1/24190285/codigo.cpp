daa72a6845a610bac533b00a4e260f4f723c44e4
Ale-985 <arteaga0615@gmail.com>
2024-06-11 20:14:37 -0500

    Update codigo.cpp

---
+++ tareas/1/24190285/codigo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,suma=0;
+    cout<<"Ingrese el primer valor: ";
+    cin>>a;
+    cout<<"Ingrese el segundo valor: ";
+    cin>>b;
+    suma=a+b;
+    cout<<"La suma es: "<<endl;
+    return 0;
+}
