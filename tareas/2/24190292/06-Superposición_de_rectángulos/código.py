98cf8cf2debd2f83ab1b5f9661079c7621166d2e
Rodrigo <jamirvac@gmail.com>
2024-07-02 14:02:24 -0500

    tarea2-2

---
+++ tareas/2/24190292/06-Superposición_de_rectángulos/código.py
@@ -0,0 +1,20 @@
+def main():
+    
+    def superponenrectángulos(a,b,alto_1,ancho_1,x,y,alto_2,ancho_2):
+        if y > b + alto_1 or b > y + alto_2:
+            return False
+        if a + ancho_1 < x or x + ancho_2 < a:
+            return False
+        if a < x and x + ancho_2 < a + ancho_1 and y + alto_2 < b + alto_1 and b < y:
+            return False
+        if a > x and x + ancho_2 > a + ancho_1 and y + alto_2 > b + alto_1 and b > y:
+            return False
+        return True
+    
+    if superponenrectángulos(-3,-2,3,4,-5,2,3,3):
+        print("verdad")
+    else:
+        print("falso")
+    
+    
+
