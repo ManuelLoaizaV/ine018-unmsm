f58c45db5a7b37db9d3d3218fd20b30cec060747
Rodrigo <jamirvac@gmail.com>
2024-07-07 20:11:29 -0500

    Primera prueba

--- tareas/2/24190292/06-Superposición_de_rectángulos/código.py
+++ tareas/2/24190292/06-Superposición_de_rectángulos/código.py
@@ -1,6 +1,15 @@
-def main():
-    
-    def superponenrectángulos(a,b,alto_1,ancho_1,x,y,alto_2,ancho_2):
+import re
+def LeerDocumento(archivoRuta):
+    with open(archivoRuta , 'r') as archivo:
+        contenido = archivo.read()
+    return contenido
+
+def ExtraigoNumeros(contenido):
+    numeros= re.findall(r'-?\d+',contenido)
+    numeros = [int(num) for num in contenido]
+    return numeros
+
+def superponenrectángulos(a,b,alto_1,ancho_1,x,y,alto_2,ancho_2):
         if y > b + alto_1 or b > y + alto_2:
             return False
         if a + ancho_1 < x or x + ancho_2 < a:
@@ -10,12 +19,21 @@ def main():
         if a > x and x + ancho_2 > a + ancho_1 and y + alto_2 > b + alto_1 and b > y:
             return False
         return True
+
+def Salida(archivoRuta,texto):
+    with open(archivoRuta,'w') as archivo:
+        archivo.write(texto)
+
+def main():
+    archivoRuta = 'entrada.txt'
+    contenido = LeerDocumento(archivoRuta)
+    n = extraigoNumeros(contenido)
     
-    if superponenrectángulos(-3,-2,3,4,-5,2,3,3):
-        print("verdad")
+    if superponenrectángulos(n[0],n[1],n[2],n[3],n[4],n[5],n[6],n[7]):
+        texto="Los rectánglos se intersecan? -> verdad"
     else:
-        print("falso")
-    
-    
+        texto="Los rectánglos se intersecan? -> falso"
+    Salida(archivoRuta,texto)
+
 if __name__ == "__main__":
     main()
