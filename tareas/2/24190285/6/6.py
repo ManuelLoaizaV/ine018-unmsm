bac743f784b2238fe9c3456d2eae8f1437a5220b
Ale-985 <arteaga0615@gmail.com>
2024-07-12 00:16:37 -0500

    6

---
+++ tareas/2/24190285/6/6.py
@@ -0,0 +1,8 @@
+with open( '6entrada.txt', "r") as entrada:
+  contenido = entrada.read().strip()
+  
+  c=int(contenido)
+conversion = (9 *c / 5)+ 32
+
+with open("6salida.txt", "w") as salida:
+       salida.write(str(conversion))
