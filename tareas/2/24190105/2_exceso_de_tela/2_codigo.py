1bf3de9125dadcd06dab2b2fa57230f8dce41bc5
lizbeth-rosio <lizbeth.mamanic@unmsm.edu.pe>
2024-07-12 17:46:07 -0500

    Update 2_codigo.py

--- tareas/2/24190105/2_exceso_de_tela/2_codigo.py
+++ tareas/2/24190105/2_exceso_de_tela/2_codigo.py
@@ -1,1 +1,19 @@
+def ObtenerYardas(pulgadas):
+    yardas=pulgadas//36
+    if pulgadas%36>0:
+      yardas+=1
+    return yardas
 
+def  ObtenerExcesoDeTela(pulgadas): 
+     return 36 * ObtenerYardas(pulgadas) - pulgadas 
+
+def ReporteCompraDeTela(pulgadas):
+    with open('2_salida.txt', 'w') as salida:
+       salida.write("Necesitamos: " + str(pulgadas) + " pulgada(s)\n" 
+      "Yardas a comprar: " + str(ObtenerYardas(pulgadas)) +"\n"
+      "Exceso de tela: "  + str (ObtenerExcesoDeTela(pulgadas)) + " pulgadas(s)\n")
+
+with open ('2_entrada.txt', 'r') as entrada:
+  pulgadas = int(entrada.readline()) 
+
+  ReporteCompraDeTela(pulgadas)
