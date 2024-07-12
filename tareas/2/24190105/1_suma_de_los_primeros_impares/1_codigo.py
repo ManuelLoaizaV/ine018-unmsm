20f45645fcea7fb22bb08bb3fd10e6c06023b969
lizbeth-rosio <lizbeth.mamanic@unmsm.edu.pe>
2024-07-12 00:44:40 -0500

    Create 1_codigo.py

---
+++ tareas/2/24190105/1_suma_de_los_primeros_impares/1_codigo.py
@@ -0,0 +1,9 @@
+with open ('1_entrada.txt', 'r') as entrada:
+  n = int(entrada.readline()) 
+
+suma = n*n
+termino_n = 2*n - 1 
+
+with open('1_salida.txt', 'w') as salida:
+     salida.write("La suma de 1 + ... + " + str(termino_n) + " es " + str(suma))
+ 
