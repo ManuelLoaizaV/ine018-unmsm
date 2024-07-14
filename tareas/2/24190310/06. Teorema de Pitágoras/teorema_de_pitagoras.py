9677e33b5df4f15a385f5fbd4d4516e080a08b05
Rosa Manrique <168355723+RosaManrique@users.noreply.github.com>
2024-07-14 18:20:32 -0500

    Tarea 2

---
+++ tareas/2/24190310/06. Teorema de Pitágoras/teorema_de_pitagoras.py
@@ -0,0 +1,7 @@
+import math
+print("Ingrese los valores de los catetos: ")
+a=float(input(""))
+b=float(input(""))
+
+c=math.sqrt(a * a + b * b)
+print("Por el teorema de Pitágoras, la hipotenusa mide", c)
