044ca851142ef8196051630f598c90461e027f6d
adolfo31o <angel.calero.requ3na@gmail.com>
2024-07-15 23:39:55 -0500

    ya quedo

--- tareas/2/24190098/LISTA_6/teorema_de_pitagoras.py
+++ tareas/2/24190098/LISTA_6/teorema_de_pitagoras.py
@@ -5,3 +5,4 @@ b = float(input())
 # Fue interesante que en python si se puede elevar un numero usando solo (**)
 c = math.sqrt(a*2 + b*2)
 print(f"Por el teorema de Pitagoras, la hipotenusa mide {c}")
+#hola
