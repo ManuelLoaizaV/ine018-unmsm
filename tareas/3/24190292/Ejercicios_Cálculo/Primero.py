75179aa6c6a114ddfcb5742cdd89bfbabfd47579
Rodrigo <jamirvac@gmail.com>
2024-07-17 12:07:21 -0500

    tarea3-códigos

--- tareas/3/24190292/Ejercicios_Cálculo/Primero.py
+++ tareas/3/24190292/Ejercicios_Cálculo/Primero.py
@@ -1,1 +1,18 @@
-print("dasdfasfafafafafafaf")
+#importamos las funcioens que necesitamos
+from sympy import  real_root , diff, parse_expr, Pow
+from sympy.abc import x,y
+# colocamos la función
+Ec = parse_expr('((x-27)/(x+8)+sin(sin(4*x)**2)**3)**(1/3)')
+#derivamos
+derivada = diff(Ec)
+derivadaencero = derivada.subs(x,0) #la derivada cuando x = 0
+derivadaencero = real_root(derivadaencero) # lo colocamos en reales 
+tangente = -Pow(derivadaencero,-1)# se calula la pendiente de la normal a partir de la derivada
+# La ecuación de la recta es 0= m * x + b
+Ecencero = Ec.subs(x,0) #b = función cuando x=0
+#formamos la ecuación
+Ecuacion = tangente*x + Ecencero 
+Ecuacion = real_root(Ecuacion) # por si Encero tmb podria ser una solución complejo
+
+print("La ecuación de la normal cuando x = 0 es:")
+print('y = ',Ecuacion)
---
+++ tareas/3/24190292/Ejercicios_Cálculo/Tercero.py
@@ -0,0 +1,16 @@
+#importamos las funciones
+from sympy import diff, parse_expr
+from sympy.abc import x,y
+
+F = parse_expr('x**2*sin(x+y)-5*y*E**x-3/sqrt(7)')
+#queremos saber su derivada implícita
+
+#calculamos la derivada conj respecto a x, y
+derivadax = diff(F,x)
+derivaday = diff(F,y)
+
+#calculamos por divisiones
+dy_dx = (-1)*derivadax/derivaday
+
+print(dy_dx)
+
