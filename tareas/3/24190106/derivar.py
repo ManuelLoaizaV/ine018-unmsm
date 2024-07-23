566f09c65334731cfa90d65fc6c5e37c30d0fd7e
Erickm1927 <mayta.meneses.erick@gmail.com>
2024-07-22 20:54:59 -0500

    Tarea3
    ojalá

--- tareas/3/24190106/derivar.py
+++ tareas/3/24190106/derivar.py
@@ -1,10 +1,10 @@
 import sympy as sp
-
+#me costó bastante instalarla
 x=sp.symbols('x')
-
+#con sp.symbols por lo que entendi es que declara al x como variable
 y=(x**2 + 2*x)
-
+#aca creo mi funcion f(x)=y
 d=sp.diff(y,x)
-
+#aca derivo mi funcion y lo igualo a d
 print(d)
-
+# y finalmente imprimo el resultado de la derivada
